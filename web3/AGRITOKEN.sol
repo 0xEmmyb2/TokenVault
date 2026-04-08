4ef8bba25ecbcf0962f63248eca4003dd86bec34
0xEmmyb2 <cyubahiroemmy12@gmail.com>
2026-04-08 21:11:50 +0200

    Created the Token handler contract

---
+++ web3/AGRITOKEN.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract AGRITOKEN is ERC20, Ownable {
+    
+}
