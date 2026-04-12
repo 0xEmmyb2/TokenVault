db300ebeb5b9b4af4a63f3386033e338f74082ac
0xEmmyb2 <cyubahiroemmy12@gmail.com>
2026-04-12 13:00:26 +0200

    Configured the _app.js file for the project

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import "..styles/globals.css"; 
+import { config } from "../provider/wagmiConfig";
+
+import {RainbowKitProvider, darkTheme} from "@rainbow-me/rainbowkit";
+import {WagmiProvider} from "wagmi";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
+
+import { Web3Provider } from "../context/Web3Provider";
+import { ToastProvider } from "../context/ToastContext";
+
+
+const queryClient = new QueryClient();
+
+function MyApp({Component, pageProps}) {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClient client={queryClient}>
+                <RainbowKitProvider theme={darkTheme({
+                    accentColor: "#45ef56",
+                    accentColorForeground: "white",
+                    borderRadius: "small",
+                    fontStack: "system",
+                    overlayBlur: "small"
+                })}>
+                    <ToastProvider>
+                        <Web3Provider>
+                            <Component {...pageProps} />
+                        </Web3Provider>
+                    </ToastProvider>
+                </RainbowKitProvider>
+            </QueryClient>
+        </WagmiProvider>
+    );
+}
